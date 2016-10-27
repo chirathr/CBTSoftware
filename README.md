81e566707f7852528a99b9e544380206fe278ae4
Chirath02 <chirath.02@gmail.com>
2016-10-27 16:41:50 +0530

    Started with CBT

---
+++ README.md
@@ -0,0 +1,2 @@
+#2048 game
+A java implemetation of the 2048 game.
---
+++ src/main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Game2048 {
+
+  public static void main(String[] args) {
+
+  }
+}
