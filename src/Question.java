9ce0c1ac58b032eeb951643c83bbba04695dc9b2
Chirath02 <chirath.02@gmail.com>
2016-10-27 16:50:22 +0530

    Create constructors for Question class

--- src/Question.java
+++ src/Question.java
@@ -8,11 +8,35 @@
 
 
 class Question {
-    int questionNumber, type, numberOfChoices, mcqAnswer[5];
+    int questionNumber, type, numberOfChoices, mcqAnswer[5], mcqAns;
     String question;
     String[] options = new String[5];
-    String trueOrFalseAnswer;
+    boolean trueOrFalseAnswer;
     String fillInTheBlankAnswer;
 
+    Question(int qno, String q, int nofc, String options[], int mcqAns) {
+        // mcq(single answer)
+        this.type = 1;
+        this.questionNumber = qno;
+
+    }
+    Question(int qno, String q, int nofc, String options[], int mcqAns[]) {
+        //mcq(multiple answers)
+        this.type = 2;
+        this.questionNumber = qno;
+
+    }
+    Question(int qno, String q, boolean torF) {
+        // True or False
+        this.type = 3;
+        this.questionNumber = qno;
+
+    }
+    Question(int qno, String q, String ans) {
+        // Fill in the blanks
+        this.type = 4;
+        this.questionNumber = qno;
+
+    }
 
 }
