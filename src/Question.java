a1675f8c1f9869ac2d19f837cdfa85b79e28b959
Chirath02 <chirath.02@gmail.com>
2016-10-27 17:00:04 +0530

    completed contructors for Question class

--- src/Question.java
+++ src/Question.java
@@ -6,7 +6,6 @@
         4 = fill in the blanks
 */
 
-
 class Question {
     int questionNumber, type, numberOfChoices, mcqAnswer[5], mcqAns;
     String question;
@@ -18,25 +17,32 @@ class Question {
         // mcq(single answer)
         this.type = 1;
         this.questionNumber = qno;
-
+        this.question = q;
+        this.numberOfChoices = nofc;
+        this.options = options;
+        this.mcqAns = mcqAns;
     }
     Question(int qno, String q, int nofc, String options[], int mcqAns[]) {
         //mcq(multiple answers)
         this.type = 2;
         this.questionNumber = qno;
-
+        this.question = q;
+        this.numberOfChoices = nofc;
+        this.options = options;
+        this.mcqAnswer = mcqAns;
     }
     Question(int qno, String q, boolean torF) {
         // True or False
         this.type = 3;
         this.questionNumber = qno;
-
+        this.question = q;
+        this.trueOrFalseAnswer = torF;
     }
     Question(int qno, String q, String ans) {
         // Fill in the blanks
         this.type = 4;
         this.questionNumber = qno;
-
+        this.question = q;
+        this.fillInTheBlankAnswer = ans;
     }
-
 }
