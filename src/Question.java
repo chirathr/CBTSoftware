2474f44a2a9df995bd2378c2cf2cf2bf916e9a44
Chirath02 <chirath.02@gmail.com>
2016-10-28 23:33:14 +0530

    Added get functions in question class

--- src/Question.java
+++ src/Question.java
@@ -7,29 +7,49 @@
 */
 
 class Question {
-    int questionNumber, type, numberOfChoices, mcqAnswer[5], mcqAns;
+    int questionNumber, type, numberOfChoices, mcqAnswer[5], mcqAnswer;
     String question;
     String[] options = new String[5];
     boolean trueOrFalseAnswer;
     String fillInTheBlankAnswer;
+    
+    public int getQuestionNumber() {
+        return questionNumber;
+    }
 
-    Question(int qno, String q, int nofc, String options[], int mcqAns) {
+    public int getType() {
+        return type;
+    }
+
+    public int getNumberOfChoices() {
+        return numberOfChoices;
+    }
+
+    public int[] getMcqAnswers() {
+       return mcqAnswers;
+    }
+
+    public int getMcqAnswer() {
+        return mcqAnswer;
+    }
+
+    Question(int qno, String q, int nofc, String options[], int mcqAnswer) {
         // mcq(single answer)
         this.type = 1;
         this.questionNumber = qno;
         this.question = q;
         this.numberOfChoices = nofc;
         this.options = options;
-        this.mcqAns = mcqAns;
+        this.mcqAnswer= mcqAnswer;
     }
-    Question(int qno, String q, int nofc, String options[], int mcqAns[]) {
+    Question(int qno, String q, int nofc, String options[], int mcqAnswer[]) {
         //mcq(multiple answers)
         this.type = 2;
         this.questionNumber = qno;
         this.question = q;
         this.numberOfChoices = nofc;
         this.options = options;
-        this.mcqAnswer = mcqAns;
+        this.mcqAnswer = mcqAnswer;
     }
     Question(int qno, String q, boolean torF) {
         // True or False
@@ -45,4 +65,6 @@ class Question {
         this.question = q;
         this.fillInTheBlankAnswer = ans;
     }
+    
+    
 }
