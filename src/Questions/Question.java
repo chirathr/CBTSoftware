340e2b810dcb3ebb8b76f5de54a1495f9d13e23f
Chirath02 <chirath.02@gmail.com>
2016-11-12 23:38:11 +0530

    added 4 setters for setting questions

--- src/Questions/Question.java
+++ src/Questions/Question.java
@@ -93,7 +93,7 @@ public class Question {
     
     // Constructors
 
-    Question(int qno, String q, int nofc, String options[], int mcqAnswer) {
+    public void setMCQSingle(int qno, String q, int nofc, String options[], int mcqAnswer) {
         // mcq(single answer)
         this.type = 1;
         this.questionNumber = qno;
@@ -102,7 +102,8 @@ public class Question {
         this.options = options;
         this.mcqAnswer= mcqAnswer;
     }
-    Question(int qno, String q, int nofc, String options[], int mcqAnswers[]) {
+    
+    public void setMCQMultiple(int qno, String q, int nofc, String options[], int mcqAnswers[]) {
         //mcq(multiple answers)
         this.type = 2;
         this.questionNumber = qno;
@@ -111,14 +112,14 @@ public class Question {
         this.options = options;
         this.mcqAnswers = mcqAnswers;
     }
-    Question(int qno, String q, boolean torF) {
+    public void setTrueOrFalse(int qno, String q, boolean torF) {
         // True or False
         this.type = 3;
         this.questionNumber = qno;
         this.question = q;
         this.trueOrFalseAnswer = torF;
     }
-    Question(int qno, String q, String ans) {
+    public void setFillInTheBlank(int qno, String q, String ans) {
         // Fill in the blanks
         this.type = 4;
         this.questionNumber = qno;
