808a265d3e0c2e2b8cb52b72a9b8cbbb46c04d2c
Chirath02 <chirath.02@gmail.com>
2016-11-12 14:18:06 +0530

    create teacher class

--- src/User/Teacher.java
+++ src/User/Teacher.java
@@ -9,6 +9,26 @@ package User;
  *
  * @author chirath
  */
-public class Teacher {
+
+public class Teacher extends Person {
+    String department; 
+
+    public Teacher(String name, String username, String password, String email, String dep) {
+        super(name, username, password, email);
+        this.department = dep;
+    }
+
+    public Teacher() {
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
     
 }
