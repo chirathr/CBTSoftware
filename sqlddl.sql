6296a150d75ae130d44bcba0a0a072cdc23d7b9c
Chirath02 <chirath.02@gmail.com>
2016-11-12 14:22:21 +0530

    Teacher ddl statements

--- sqlddl.sql
+++ sqlddl.sql
@@ -9,3 +9,13 @@ create table Student(
 
 insert into student values(1, 'Chirath R', 'chirath', '1234', 'chirath.02@gmail.com', 5);
 
+create table Teacher(
+    id numeric(10) primary key,
+    name varchar(100),
+    username varchar(100),
+    password varchar(100),
+    email varchar(100),
+    depatment varchar(20)
+);
+
+insert into teacher values(1, 'Chirath R', 'chirath', '1234', 'chirath.02@gmail.com', 'CSE');
--- src/User/Teacher.java
+++ src/User/Teacher.java
@@ -5,6 +5,12 @@
  */
 package User;
 
+import Dbconnection.PSQLConnect;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author chirath
@@ -29,6 +35,66 @@ public class Teacher extends Person {
         this.department = department;
     }
     
-    
+    public boolean login(String username, String password) {
+        List<List<String>> result = null;
+        PSQLConnect psql = new PSQLConnect();
+        psql.connectPSQL();
+        try {
+            String query = "select * from teacher where username = '" 
+                    + username + "' and password = '" + password + "';";
+            result = psql.runPSQLQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, 
+                    null, ex);
+        }
+        if(result.size() == 1) {
+            System.out.println("Login successful");
+            return true;
+        }
+        return false;
+    }
+    public boolean save(
+            String name, 
+            String username, 
+            String password, 
+            String email, 
+            int semster
+        ) {
+        
+        List<List<String>> result = null;
+        PSQLConnect psql = new PSQLConnect();
+        try {
+            String query = "select * from teacher where username = '" 
+                    + username + "';";
+            psql.connectPSQL();
+            result = psql.runPSQLQuery(query);
+            if(result.size() == 1) {
+                System.out.println("username already exists!");
+                return false;
+            }
+            query = "select * from teacher where email = '" + email + "';";
+            psql.connectPSQL();
+            result = psql.runPSQLQuery(query);
+            if(result.size() == 1) {
+                System.out.println("email already exists!");
+                return false;
+            }
+            query = "select max(id) from student;";
+            psql.connectPSQL();
+            result = psql.runPSQLQuery(query);
+            int nextId = Integer.parseInt(result.get(0).get(0)) + 1;
+            query = "insert into teacher values(" + nextId + ", '" + 
+                    name + "', '" + username + "', '" + password +
+                    "', '" + email + "', " + semster + ");";
+            psql.connectPSQL();
+            psql.insertQuery(query);
+            System.out.println("User created successfully!");
+        } catch (SQLException ex) {
+            Logger.getLogger(
+                    Student.class.getName()).log(Level.SEVERE, 
+                            null, ex);
+        }
+        return true;
+    }
     
 }
