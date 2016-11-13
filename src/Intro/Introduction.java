32430ac1481b4f5ddcb5c5c8a7b19bf3b8ad3b53
Chirath02 <chirath.02@gmail.com>
2016-11-13 09:24:02 +0530

    Created readfrom file, intro and taketest

---
+++ src/Intro/Introduction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Intro;
+
+/**
+ *
+ * @author chirath
+ */
+public class Introduction {
+    public void displayInfo() {
+        //Display intro, call this function in CBTSofware
+    }
+    public void displayRules() {
+        //Display rules, call this function in CBTSoftware
+    }
+}
